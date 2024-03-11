5004307b92ac227a378fffaa22530d85ab8a651b
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-03-11 15:23:38 +0000

    second one

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 function Header() {
-  return <div>Header</div>;
+  // Armazenar o estado do Redux
+  const userInfo = useSelector((state: any) => state.user);
+
+  return (
+    <header>
+      <h3 data-testid="email-field">{userInfo.email}</h3>
+      <h3 data-testid="total-field">0</h3>
+      <h3 data-testid="header-currency-field">BRL</h3>
+    </header>
+  );
 }
 
 export default Header;
--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,5 +1,12 @@
+import Header from '../components/Header';
+
 function Wallet() {
-  return <div>TrybeWallet</div>;
+  return (
+    <div>
+      <Header />
+      <h1>Wallet</h1>
+    </div>
+  );
 }
 
 export default Wallet;
