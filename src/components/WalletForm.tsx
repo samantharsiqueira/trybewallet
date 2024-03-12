ac85b311ba062af2692cd8dbad0bab6fad36da4b
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-03-12 12:44:55 +0000

    updating 3

--- src/components/WalletForm.tsx
+++ src/components/WalletForm.tsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { AnyActionWallet } from 'redux';
+import { AnyAction } from 'redux'; // Como ajustar a tipagem??
 
 function WalletForm() {
-  const dispatch = useDispatch();
-  const currencies = useSelector((state: AnyActionWallet) => state.currencies);
-  const [value, setValue] = useState('');
-  const [description, setDescription] = useState('');
-  const [selectedCurrency, setSelectedCurrency] = useState('USD'); // Valor inicial, pode ser ajustado conforme necessário
-  const [method, setMethod] = useState('Dinheiro'); // Valor inicial, pode ser ajustado conforme necessário
+  const dispatch = useDispatch(); // Evia pro estado global
+  const currencies = useSelector((state: AnyAction) => state.currencies); // Seleciona a moeda e guarda em currencies
+  const [value, setValue] = useState(''); // Valor inicial do input valor
+  const [description, setDescription] = useState(''); // Valor inicial da descrição
+  const [selectedCurrency, setSelectedCurrency] = useState('USD'); // Valor inicial da moeda
+  const [method, setMethod] = useState('Dinheiro'); // Valor inicial do método de pagamento
   const [tag, setTag] = useState('Alimentação'); // Valor inicial, pode ser ajustado conforme necessário
 
   useEffect(() => {
@@ -17,9 +17,9 @@ function WalletForm() {
       try {
         const response = await fetch('https://economia.awesomeapi.com.br/json/all');
         const data = await response.json();
-        const currenciesArray = Object.keys(data)
-          .filter((currency) => currency !== 'USDT');
-        dispatch({ type: 'SET_CURRENCIES', payload: currenciesArray });
+        const currenciesArray = Object.keys(data) // Pega as chaves do objeto
+          .filter((currency) => currency !== 'USDT'); // Filtra as moedas que não são USDT
+        dispatch({ type: 'SET_CURRENCIES', payload: currenciesArray }); // Envia as moedas para o estado global
       } catch (error) {
         console.error('Erro ao obter moedas da API', error);
       }
@@ -49,18 +49,21 @@ function WalletForm() {
 
   return (
     <>
+      {/* Valor */}
       <input
         data-testid="value-input"
         type="text"
         value={ value }
-        onChange={ (e) => setValue(e.target.value) }
+        onChange={ (e) => setValue(e.target.value) } // Atualiza o estado com o valor do input
       />
+      {/* Descrição */}
       <input
         data-testid="description-input"
         type="text"
         value={ description }
-        onChange={ (e) => setDescription(e.target.value) }
+        onChange={ (e) => setDescription(e.target.value) } // Atualiza o estado com o valor da descrição
       />
+      {/* Moeda */}
       <select
         data-testid="currency-input"
         value={ selectedCurrency }
@@ -72,19 +75,21 @@ function WalletForm() {
           </option>
         ))}
       </select>
+      {/* Método de pagamento */}
       <select
         data-testid="method-input"
         value={ method }
-        onChange={ (e) => setMethod(e.target.value) }
+        onChange={ (e) => setMethod(e.target.value) } // Atualiza o estado com o valor do método de pagamento
       >
         <option value="Dinheiro">Dinheiro</option>
         <option value="Cartão de crédito">Cartão de crédito</option>
         <option value="Cartão de débito">Cartão de débito</option>
       </select>
+      {/* Categoria de despesa */}
       <select
         data-testid="tag-input"
-        value={ tag }
-        onChange={ (e) => setTag(e.target.value) }
+        value={ tag } // Valor que estamos selecionando no momento
+        onChange={ (e) => setTag(e.target.value) } // Atualiza o estado com o valor da categoria de despesa
       >
         <option value="Alimentação">Alimentação</option>
         <option value="Lazer">Lazer</option>
