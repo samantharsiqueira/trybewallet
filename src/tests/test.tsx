88187ea1813007c1d085d3e585247ec35d14b7a1
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-03-12 20:36:22 +0000

    updating

--- src/tests/test.tsx
+++
@@ -1,9 +0,0 @@
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { vi } from 'vitest';
-import { renderWithRouter, renderWithRedux, renderWithRouterAndRedux } from './helpers/renderWith';
-import App from '../App';
-
-const LOGIN_ROUTE = '/';
-const WALLET_ROUTE = '/carteira';
-
